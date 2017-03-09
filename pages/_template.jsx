42619f887ba7fa9915cd8c986e6532e206388038
Rusta <rustauk@gmail.com>
2017-03-09 07:44:02 +0000

    lint proof the template file

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 import '../src/css/styles.css'
 
-import PageLink from '../src/components/common/PageLink'
 import SiteHeader from '../src/components/SiteHeader'
 
-module.exports = React.createClass({
-  propTypes() {
-    return {
-      children: React.PropTypes.any,
-    }
-  },
-  render() {
-    const activePath = this.props.location.pathname
-    return (
+const Template = ({ location, children }) => {
+  const activePath = location.pathname
+  return (
+    <div>
+      <SiteHeader activePath={activePath} />
       <div>
-        <SiteHeader activePath={activePath} />
-        <div>
-          {this.props.children}
-        </div>
+        {children}
       </div>
-    )
-  },
-})
+    </div>
+  )
+}
+
+Template.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  children: PropTypes.any.isRequired,
+}
+
+export default Template
